9a03dd46e9ab6cba8744ead14d09624de5da0a84
GitRedaAmine <redatlse18@gmail.com>
2021-03-09 14:23:59 +0100

    test event binding  angular chechpoint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TwoWayDataBindingComponent } from "./two-way-data-binding/two-way-data-binding.component";
+import { EventBindingComponent } from "./event-binding/event-binding.component";
+
+
+const routes: Routes = [
+  {path:'EventBinding', component : EventBindingComponent},
+  {path:'TwoWayDataBindingComponent', component : TwoWayDataBindingComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
