0cb37853d0f9a474e2f0976b1c84627fdc98a2f3
GitRedaAmine <redatlse18@gmail.com>
2021-04-10 16:40:38 +0100

    update ngtyle checkpoints

--- src/app/AttributeDirectivesNgStyl/attribute-directives-ng-styl/attribute-directives-ng-styl.component.ts
+++ src/app/AttributeDirectivesNgStyl/attribute-directives-ng-styl/attribute-directives-ng-styl.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./attribute-directives-ng-styl.component.css']
 })
 export class AttributeDirectivesNgStylComponent implements OnInit {
+  
+  FontNameList! : string[];
+  SelectedFont! :string;
 
-  constructor() { }
+  CurrentColor!:string; 
+  CurrentSize!:number; 
+  
+  constructor() {
+    this.FontNameList = [
+      "serif",
+      "sans-serif",
+      "monospace",
+      "cursive",
+      "fantasy",
+      "system-ui",
+      "emoji",
+      "math",
+      "fangsong"
+   
+    ]
+    this.SelectedFont=this.FontNameList[1];
+    this.CurrentColor="black";
+    this.CurrentSize=25;
+   }
 
   ngOnInit(): void {
   }
+   getFontSize(): string {
+      return  this.CurrentSize +"px";
+  }
+  getFontColor(): string {
+    return  this.CurrentColor ;
+}
+  getFontFamilly(): string {
+    return  this.SelectedFont ;
+}
 
 }
