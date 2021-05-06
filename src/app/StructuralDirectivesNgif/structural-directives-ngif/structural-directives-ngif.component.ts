2bc4adfce1f28ac3a3b8ae43aa32b417c5730de9
GitRedaAmine <redatlse18@gmail.com>
2021-05-06 01:02:15 +0100

    structural-directives-ngif Checkpoint

--- src/app/StructuralDirectivesNgif/structural-directives-ngif/structural-directives-ngif.component.ts
+++ src/app/StructuralDirectivesNgif/structural-directives-ngif/structural-directives-ngif.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./structural-directives-ngif.component.css']
 })
 export class StructuralDirectivesNgifComponent implements OnInit {
-
+  isToDisplay :boolean =true; 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onClick(){    
+    console.log('old isToDisplay : '+this.isToDisplay)
+    this.isToDisplay=!this.isToDisplay;
+    console.log('new  isToDisplay : '+this.isToDisplay)
+  }    
 }
